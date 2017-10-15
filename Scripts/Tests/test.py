6b3c4dde8b30b17b2b8daa35c4028386123c766f
Jimmy Le <JLDevOps@gmail.com>
2017-10-14 20:40:08 -0400

    Removed test.py

--- Scripts/Tests/test.py
+++
@@ -1,7 +0,0 @@
-from unittest import TestCase
-
-
-class test_auth_scrape(TestCase):
-    def test_scrape(self):
-        auth_scrape(
-            '/Users/destinesavior/Documents/Hidden-Desktop/Hidden-Desktop/Project/Auth-Port-Scanner/Logs/auth.log')
